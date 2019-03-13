7ece6faa5f6b9df04b60db197f3fb8d700f0966e
Rémy Vandaele <remy.vandaele@gmail.com>
2019-03-13 14:09:53 +0100

    move functionalities to LandmarkTools

--- run.py
+++ run.py
@@ -22,13 +22,6 @@ __contributors__ = ["Marée Raphaël <raphael.maree@ulg.ac.be>"]
 __copyright__ = "Copyright 2010-2016 University of Liège, Belgium, http://www.cytomine.be/"
 
 from neubiaswg5.helpers.data_upload import imwrite
-from sklearn.externals import joblib
-from array import *
-from shapely.geometry.polygon import Polygon
-from shapely.geometry import Point
-import sys, optparse
-import cytomine
-from cytomine import cytomine, models
 from cytomine.models import *
 import numpy as np
 from ldmtools import *
@@ -36,7 +29,6 @@ from scipy.stats import multivariate_normal
 from sumproduct import Variable,Factor,FactorGraph
 from ldmtools import *
 import numpy as np
-from multiprocessing import Pool
 import scipy.ndimage as snd
 from sklearn.externals import joblib
 import sys, os
@@ -44,111 +36,6 @@ from neubiaswg5 import CLASS_LNDDET
 from neubiaswg5.helpers import NeubiasJob, prepare_data, get_discipline
 from cytomine.models import Job, AttachedFile, Property
 import joblib
-from sklearn.tree import ExtraTreeClassifier, ExtraTreeRegressor
-import imageio
-
-"""
-Given the classifier clf, this function will try to find the landmark on the
-image current
-"""
-
-
-def searchpoint_cytomine(repository, current, clf, mx, my, cm, depths, window_size, feature_type, feature_parameters, image_type, npred):
-	simage = readimage(repository, current, image_type, image_type='tif')
-	(height, width) = simage.shape
-	P = np.random.multivariate_normal([mx, my], cm, npred)
-	x_v = np.round(P[:, 0] * width)
-	y_v = np.round(P[:, 1] * height)
-	height = height - 1
-	width = width - 1
-	n = len(x_v)
-	pos = 0
-	maxprob = -1
-	maxx = []
-	maxy = []
-
-	# maximum number of points considered at once in order to not overload the
-	# memory.
-	step = 100000
-
-	for index in range(len(x_v)):
-		xv = x_v[index]
-		yv = y_v[index]
-		if (xv < 0):
-			x_v[index] = 0
-		if (yv < 0):
-			y_v[index] = 0
-		if (xv > width):
-			x_v[index] = width
-		if (yv > height):
-			y_v[index] = height
-
-	while (pos < n):
-		xp = np.array(x_v[pos:min(n, pos + step)])
-		yp = np.array(y_v[pos:min(n, pos + step)])
-
-		DATASET = build_dataset_image(simage, window_size, xp, yp, feature_type, feature_parameters, depths)
-		pred = clf.predict_proba(DATASET)
-		pred = pred[:, 1]
-		maxpred = np.max(pred)
-		if (maxpred >= maxprob):
-			positions = np.where(pred == maxpred)
-			positions = positions[0]
-			xsup = xp[positions]
-			ysup = yp[positions]
-			if (maxpred > maxprob):
-				maxprob = maxpred
-				maxx = xsup
-				maxy = ysup
-			else:
-				maxx = np.concatenate((maxx, xsup))
-				maxy = np.concatenate((maxy, ysup))
-		pos = pos + step
-
-	return np.median(maxx), (height + 1) - np.median(maxy)
-
-
-def str2bool(v):
-	return v.lower() in ("yes", "true", "t", "1")
-
-def dataset_from_coordinates(img,x,y,feature_offsets):
-	(h,w) = img.shape
-	original_values = img[y.clip(min=0,max=h-1),x.clip(min=0,max=w-1)]
-	dataset = np.zeros((x.size,feature_offsets[:,0].size))
-	for i in range(feature_offsets[:,0].size):
-		dataset[:,i] = original_values-img[(y+feature_offsets[i,1]).clip(min=0,max=h-1),(x+feature_offsets[i,0]).clip(min=0,max=w-1)]
-	return dataset
-
-def probability_map_phase_1(repository, image_number, clf, feature_offsets, delta):
-	img = makesize(snd.zoom(readimage(repository, image_number, image_type='tif'), delta), 1)
-	(h, w) = img.shape
-
-	c = np.arange((h - 2) * (w - 2))
-	ys = 1 + np.floor(c / (w - 2)).astype('int')
-	xs = 1 + np.mod(c, (w - 2))
-
-	step = 20000
-	b = 0
-	probability_map = None
-	nldms = -1
-
-	while (b < xs.size):
-
-		next_b = min(b + step, xs.size)
-		# print b,next_b
-		dataset = dataset_from_coordinates(img, xs[b:next_b], ys[b:next_b], feature_offsets)
-		probabilities = clf.predict_proba(dataset)
-
-		if (nldms == -1):
-			(ns, nldms) = probabilities.shape
-			probability_map = np.zeros((h - 2, w - 2, nldms))
-
-		for ip in range(nldms):
-			probability_map[ys[b:next_b] - 1, xs[b:next_b] - 1, ip] = probabilities[:, ip]
-		b = next_b
-
-	return probability_map
-
 
 def filter_perso(img, filter_size):
 	if np.sum(img > 0) <= 1:
@@ -259,19 +146,14 @@ def compute_final_solution_phase_3(xc, yc, probability_map_phase_2, ncandidates,
 
 	b_mat = build_bmat_phase_3(xc, yc, T, x_candidates, y_candidates, edges, sde)
 
-	# (hh,ww,dd) = b_mat.shape
-
 	g = FactorGraph(silent=True)
 	nodes = [Variable('x%d' % i, len(x_candidates[i])) for i in range(nldms)]
-	# print b_mat.shape,ncandidates
 	for ip in range(nldms):
 		for ipl in edges[ip, :].astype(int):
 			g.add(Factor('f2_%d_%d' % (ip, ipl), b_mat[(ip, ipl)]))
 			g.append('f2_%d_%d' % (ip, ipl), nodes[ip])
 			g.append('f2_%d_%d' % (ip, ipl), nodes[ipl])
 
-	# ycand = np.array(y_candidates).astype(int)
-	# xcand = np.array(x_candidates).astype(int)
 	for i in range(nldms):
 		v = probability_map_phase_2[np.array(y_candidates[i]), np.array(x_candidates[i]), i]
 		g.add(Factor('f1_%d' % i, v / np.sum(v)))
@@ -292,116 +174,6 @@ def compute_final_solution_phase_3(xc, yc, probability_map_phase_2, ncandidates,
 def find_by_attribute(att_fil, attr, val):
 	return next(iter([i for i in att_fil if hasattr(i, attr) and getattr(i, attr) == val]), None)
 
-def build_separate_tree(X, y, max_features, max_depth, min_samples_split):
-	clf = ExtraTreeClassifier(max_features=max_features, max_depth=max_depth, min_samples_split=min_samples_split)
-	clf = clf.fit(X, y)
-	return clf
-
-
-def separatetree_training_mp_helper(jobargs):
-	return build_separate_tree(*jobargs)
-
-
-def separatetree_test_mp_helper(jobargs):
-	return test_separate_tree(*jobargs)
-
-
-def test_separate_tree(tree, X):
-	return tree.predict_proba(X)
-
-
-class SeparateTrees:
-
-	def __init__(self, n_estimators=10, max_features='auto', max_depth=None, min_samples_split=2, n_jobs=1):
-		self.n_estimators = n_estimators
-		self.max_features = max_features
-		self.max_depth = max_depth
-		self.min_samples_split = min_samples_split
-		self.n_jobs = n_jobs
-
-	def fit(self, X, y):
-		self.trees = []
-		self.n_classes = np.max(y) + 1
-
-		(h, w) = X.shape
-		n_features = w / self.n_estimators
-
-		p = Pool(self.n_jobs)
-
-		jobargs = [(X[:, int(i * n_features):int((i + 1) * n_features)], y, self.max_features, self.max_depth, self.min_samples_split) for i in range(self.n_estimators)]
-		self.trees = p.map(separatetree_training_mp_helper, jobargs)
-		p.close()
-		p.join()
-
-		return self
-
-	def predict_proba(self, X):
-		(h, w) = X.shape
-		n_features = w / self.n_estimators
-		p = Pool(self.n_jobs)
-		jobargs = [(self.trees[i], X[:, int(i * n_features):int((i + 1) * n_features)]) for i in range(self.n_estimators)]
-		probas = p.map(separatetree_test_mp_helper, jobargs)
-		p.close()
-		p.join()
-		return np.sum(probas, axis=0) / float(self.n_estimators)
-
-	def predict(self, X):
-		probas = self.predict_proba(X)
-		return np.argmax(probas, axis=1)
-
-def build_separate_tree_regressor(X, y, max_features, max_depth, min_samples_split):
-	clf = ExtraTreeRegressor(max_features=max_features, max_depth=max_depth, min_samples_split=min_samples_split)
-	clf = clf.fit(X, y)
-	return clf
-
-
-def separatetree_reg_training_mp_helper(jobargs):
-	return build_separate_tree_regressor(*jobargs)
-
-
-def separatetree_reg_test_mp_helper(jobargs):
-	return test_separate_tree_reg(*jobargs)
-
-
-def test_separate_tree_reg(tree, X):
-	return tree.predict(X)
-
-
-class SeparateTreesRegressor:
-	def __init__(self, n_estimators=10, max_features='auto', max_depth=None, min_samples_split=2, n_jobs=1):
-		self.n_estimators = n_estimators
-		self.max_features = max_features
-		self.max_depth = max_depth
-		self.min_samples_split = min_samples_split
-		self.n_jobs = n_jobs
-
-	def fit(self, X, y):
-		self.trees = []
-		self.n_classes = np.max(y) + 1
-
-		(h, w) = X.shape
-		n_features = w / self.n_estimators
-
-		p = Pool(self.n_jobs)
-		jobargs = [(X[:, int(i * n_features):int((i + 1) * n_features)], y, self.max_features, self.max_depth, self.min_samples_split) for i in range(self.n_estimators)]
-		self.trees = p.map(separatetree_reg_training_mp_helper, jobargs)
-		p.close()
-		p.join()
-
-		return self
-
-	def predict(self, X):
-		(h, w) = X.shape
-		n_features = w / self.n_estimators
-		p = Pool(self.n_jobs)
-		jobargs = [(self.trees[i], X[:, int(i * n_features):int((i + 1) * n_features)]) for i in range(self.n_estimators)]
-		probas = p.map(separatetree_reg_test_mp_helper, jobargs)
-		p.close()
-		p.join()
-		return np.sum(probas, axis=0) / float(self.n_estimators)
-
-
-
 def main():
 	with NeubiasJob.from_cli(sys.argv) as conn:
 		problem_cls = get_discipline(conn, default=CLASS_LNDDET)
@@ -415,36 +187,27 @@ def main():
 				str_terms = prop.fetch(key='id_terms').value
 		term_list = [int(x) for x in str_terms.split(' ')]
 		attached_files = AttachedFileCollection(train_job).fetch()
-
 		model_file = find_by_attribute(attached_files, "filename", "model_phase1.joblib")
 		model_filepath = os.path.join(in_path, "model_phase1.joblib")
 		model_file.download(model_filepath, override=True)
 		clf = joblib.load(model_filepath)
-
 		pr_ims = [int(p) for p in conn.parameters.cytomine_predict_images.split(',')]
-
-		#Construction des maps de la phase 1
 		offset_file = find_by_attribute(attached_files, "filename", "offsets_phase1.joblib")
 		offset_filepath = os.path.join(in_path, "offsets_phase1.joblib")
 		offset_file.download(offset_filepath, override=True)
 		feature_offsets_1 = joblib.load(offset_filepath)
-
 		train_parameters = {}
 		for hashmap in train_job.jobParameters:
 			train_parameters[hashmap['name']] = hashmap['value']
-
 		train_parameters['model_delta'] = float(train_parameters['model_delta'])
 		train_parameters['model_sde'] = float(train_parameters['model_sde'])
 		train_parameters['model_T'] = int(train_parameters['model_T'])
-
 		for j in conn.monitor(pr_ims, start=10, end=33, period=0.05,prefix="Phase 1 for images..."):
 			probability_map = probability_map_phase_1(in_path, j, clf, feature_offsets_1, float(train_parameters['model_delta']))
 			filesave = os.path.join(out_path, 'pmap_%d.npy'%j)
 			np.savez_compressed(filesave,probability_map)
 
 		clf = None
-
-		#Construction des maps de la phase 2
 
 		coords_file = find_by_attribute(attached_files, "filename", "coords.joblib")
 		coords_filepath = os.path.join(in_path, "coords.joblib")
@@ -469,7 +232,6 @@ def main():
 				filesave = os.path.join(out_path, 'pmap2_%d_%d.npy' % (j, id_term))
 				np.savez_compressed(filesave, probability_map_phase_2)
 
-		#Trouvage des solutions phase 3
 		edge_file = find_by_attribute(attached_files, "filename", "model_edges.joblib")
 		edge_filepath = os.path.join(in_path, "model_edges.joblib")
 		edge_file.download(edge_filepath, override=True)
@@ -491,5 +253,6 @@ def main():
 				lbl_img[y, x] = term_list[i]
 			imwrite(path=os.path.join(out_path, '%d.tif' % j), image=lbl_img.astype(np.uint8), is_2d=True)
 		conn.job.update(progress=100, status=Job.TERMINATED, statusComment="Job terminated.")
+
 if __name__ == "__main__":
 	main()
